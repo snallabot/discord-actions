4a30552219e7e70845fedbe41cf90afb74e23003
Sahith Nallapareddy <sahith.reddy@gmail.com>
2024-08-17 21:58:24 -0400

    refactor discord client and add firebase db (#2)

--- src/commands/league_export.ts
+++ src/commands/league_export.ts
@@ -1,10 +1,11 @@
 import { ParameterizedContext } from "koa"
 import { CommandHandler, Command } from "../commands_handler"
-import { respond, createMessageResponse } from "../discord_utils"
+import { respond, createMessageResponse, DiscordClient } from "../discord_utils"
 import { ApplicationCommandType, RESTPostAPIApplicationCommandsJSONBody } from "discord-api-types/v10"
+import { Firestore } from "firebase-admin/firestore"
 
 export default {
-    async handleCommand(command: Command, ctx: ParameterizedContext) {
+    async handleCommand(command: Command, client: DiscordClient, db: Firestore, ctx: ParameterizedContext) {
         const { guild_id } = command
         respond(ctx, createMessageResponse(`Type this URL carefully into your app (no spaces exactly as shown here): https://snallabot.herokuapp.com/${guild_id}`))
     },
